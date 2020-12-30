b05e3436d6a6a42df50d42af3cbef71a4377252c
brianqian <qian.brian@gmail.com>
2020-12-30 02:44:17 -0500

    update

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,9 +2,10 @@ import fetch from 'isomorphic-unfetch';
 
 export default async (req, res) => {
   console.log('route hit');
+  const GAME_ID = 4985152263159808;
   const { endpoint } = req.query;
   console.log(req.query);
-  const resp = await fetch(`http://nptriton.cqproject.net/game/5039362715418624/${endpoint}`);
+  const resp = await fetch(`http://nptriton.cqproject.net/game/${GAME_ID}/${endpoint}`);
   const data = await resp.json();
   console.log(data);
   res.json(data);
